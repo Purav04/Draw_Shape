e45492a111e43a0e71e1488d5b5463b99b7e4b32
Purav04 <49310456+Purav04@users.noreply.github.com>
2021-04-09 14:10:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ Created on Wed Apr  7 22:03:36 2021
 from flask import Flask,render_template
 import pytesseract
 import numpy as np
-import pyautogui
-import cv2
-
+#import pyautogui
+#import cv2
+from Xlib import display, X
+from PIL import Image #PIL
 
 app = Flask(__name__)
 
@@ -23,13 +24,25 @@ def index():
 @app.route("/predict")
 def predict():
     pytesseract.pytesseract.tesseract_cmd = "tesseract\\tesseract.exe"
-    img = pyautogui.screenshot(region=(10, 140, 1850, 700))
-    frame = np.array(img)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #img = pyautogui.screenshot(region=(10, 140, 1850, 700))
+    #frame = np.array(img)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
-    shape = pytesseract.image_to_string(frame)
+    
+
+    W,H = 200,200
+    dsp = display.Display()
+    try:
+        root = dsp.screen().root
+        raw = root.get_image(10, 140, 1850,700, X.ZPixmap, 0xffffffff)
+        image = Image.fromstring("RGB", (W, H), raw.data, "raw", "BGRX")
+    finally:
+        dsp.close()
+    
+    
+    shape = pytesseract.image_to_string(image)
     shape=[i.lower() for i in shape.split("\n") if i not in [""," ","\x0c"]]
-    print(shape)
+    #print(shape)
     
     return render_template("index.html", prediction = shape[0] if len(shape)>0 else " ")
 
