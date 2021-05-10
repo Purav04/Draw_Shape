236c8aad3009cccdf3cd17bfb10b9c8cad8e6b5c
Purav04 <49310456+Purav04@users.noreply.github.com>
2021-05-10 16:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html",prediction="")
+    return render_template("index.html")
 
 
 @app.route("/predict",methods = ['GET','POST'])
 def predict():
-    
+    # getting canvas image from js and decode it
     data_url = request.values['url']
     offset = data_url.index(',')+1
     img_bytes = base64.b64decode(data_url[offset:])
@@ -23,11 +23,14 @@ def predict():
     new_image.paste(img, (0, 0), img)              
     new_image.convert('RGB')
     
+    # start pytesseract
     pytesseract.pytesseract.tesseract_cmd = "tesseract\\tesseract.exe"
     
+    # predict from canvas image
     shape = pytesseract.image_to_string(new_image)
     shape=[i.lower() for i in shape.split("\n") if i not in [""," ","\x0c"]]
     
+    #return it to js
     return json.dumps(shape[0] if len(shape)>0 else " ")
 
 if __name__ == '__main__':
